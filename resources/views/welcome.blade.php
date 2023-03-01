dbf01c60e450970de5b58bc4ec8d92172dca3f6f
Andrea ___ <andrewlast@hotmail.it>
2023-03-01 11:33:47 +0100

    copiato materiale da vecchio progetto

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,104 @@
+<x-layout> 
+
+        <!-- HEADER -->
+        <header class="header_index">
+        
+            <section class="container-fluid">
+                
+                <div class="row">
+                    
+                    <div class="col-12 col-md-12 d-flex justify-content-center">
+                        
+                        <h1 class="text-center h1_header"> PRIMITIVO AMABILE </h1>
+                        
+                        <img src="./media/1.jpg" class="img_header" alt="">
+                        
+                    </div>
+                    
+                </div>
+                
+            </section>
+            
+        </header>
+
+
+{{-- CAROSELLO --}}
+
+<div class="carousel-item">
+    <img src="media/1.jpg" alt="...">
+    <img src="media/3copia.jpg" alt="...">
+    <div class="carousel-caption d-none d-md-block d-lg-block">
+      <h5>CIAO</h5>
+      <p>ciao</p>
+    </div>
+  </div>
+
+
+        <!-- SEZIONE TESTI SOTTO HEADER -->
+    <section class="container-fluid">
+        
+        <div class="row">
+            
+            <div class="col-12 col-md-4 text-center">
+                <p style="font-weight: bold;"> Zona di Produzione: Località Matine, nel territorio di Mottola (300 m s.l.m.).
+                    
+                    Suolo: Argilloso, ricco di scheletro.
+                    
+                    Sistema di allevamento: Guyot.
+                    
+                    Vitigni: 100% Primitivo.
+                    
+                    Produzione per ettaro: 110 q.li/ha.
+                    
+                    Epoca di vendemmia: Prima decade di Settembre. 
+                    
+                    Raccolta: Manuale. 
+                </p>
+
+            </div>
+            
+            <div class="col-12 col-md-4 text-center">
+                <h2> PRIMITIVO AMABILE </h2>
+                <p style="font-weight: bold;"> PUGLIA
+                    Indicazione Geografica Protetta
+                    Rosso
+                </p>
+            </div>
+
+            <div class="col-12 col-md-4 text-center">
+                <p style="font-weight: bold;"> Vinificazione e affinamento: Le uve Primitivo, vengono raccolte manualmente in piccoli cassoni e portate in cantina dove fermentano per circa 7 giorni. Sucessivamente il vino viene affinato in piccoli serbatoi di acciaio inox. Prima di essere messo in vendita, riposa in bottiglia per circa 2 mesi. </p>
+            </div>
+
+        </div>
+    </section>
+
+    <section class="container-fluid">
+
+        <div class="row">
+
+            <div class="col-12 col-md-4 d-flex justify-content-center">
+
+                <img src="media/casolare.jpg" class="img_casolare" alt="">
+
+            </div>
+
+        </div>
+
+    </section>
+
+
+    <!-- SEZIONE GOCCIA -->
+    <div class="container-fluid div_goccia">
+        
+        <div class="drop div_goccia"></div>
+        <div class="wave div_goccia"></div>
+        <div class="row div_goccia">
+            
+            <div class="col-12 div_goccia d-flex justify-content-center align-items-center vh-100">
+                
+            </div>
+        </div>
+    </div>
+    
+    
+</x-layout>

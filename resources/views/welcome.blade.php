b2b4d16d8ba3e75e082d7618b2f9c41599de6ad0
Andrea ___ <andrewlast@hotmail.it>
2023-03-01 12:21:06 +0100

    Struttura data alla pagina

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,46 +1,56 @@
 <x-layout> 
-
-        <!-- HEADER -->
-        <header class="header_index">
+    
+    <!-- HEADER --> {{-- CAROSELLO --}}
+    <header class="header_custom">
         
-            <section class="container-fluid">
+        <section class="container">
+            
+            <div class="row">
                 
-                <div class="row">
+                <div class="col-12">
                     
-                    <div class="col-12 col-md-12 d-flex justify-content-center">
-                        
-                        <h1 class="text-center h1_header"> PRIMITIVO AMABILE </h1>
-                        
-                        <img src="./media/1.jpg" class="img_header" alt="">
-                        
+                    <h1 class="text-center"> FIRÀ </h1>
+                    
+                    <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active" data-bs-interval="4000">
+                                <img src="media/1.jpg" class="d-block img_carousel_custom" alt="...">
+                            </div>
+                            <div class="carousel-item" data-bs-interval="4000">
+                                <img src="media/casolare.jpg" class="d-block img_carousel_custom" alt="...">
+                            </div>
+                            <div class="carousel-item" data-bs-interval="4000">
+                                <img src="media/interno.jpg" class="d-block img_carousel_custom" alt="...">
+                            </div>
+                        </div>
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button>
                     </div>
                     
                 </div>
                 
-            </section>
+            </div>
             
-        </header>
-
-
-{{-- CAROSELLO --}}
-
-<div class="carousel-item">
-    <img src="media/1.jpg" alt="...">
-    <img src="media/3copia.jpg" alt="...">
-    <div class="carousel-caption d-none d-md-block d-lg-block">
-      <h5>CIAO</h5>
-      <p>ciao</p>
-    </div>
-  </div>
-
-
-        <!-- SEZIONE TESTI SOTTO HEADER -->
-    <section class="container-fluid">
+        </section>
+        
+    </header>
+    
+    <!-- PRIMA SEZIONE SOTTO HEADER -->
+    <section class="container-fluid ">
         
         <div class="row">
             
-            <div class="col-12 col-md-4 text-center">
-                <p style="font-weight: bold;"> Zona di Produzione: Località Matine, nel territorio di Mottola (300 m s.l.m.).
+            <div class="col-12 col-md-5 text-center d-flex justify-content-center">
+
+                <p style="font-weight: bold;"> 
+                    
+                    Zona di Produzione: Località Matine, nel territorio di Mottola (300 m s.l.m.).
                     
                     Suolo: Argilloso, ricco di scheletro.
                     
@@ -54,51 +64,86 @@
                     
                     Raccolta: Manuale. 
                 </p>
-
-            </div>
-            
-            <div class="col-12 col-md-4 text-center">
-                <h2> PRIMITIVO AMABILE </h2>
-                <p style="font-weight: bold;"> PUGLIA
-                    Indicazione Geografica Protetta
-                    Rosso
-                </p>
-            </div>
-
-            <div class="col-12 col-md-4 text-center">
-                <p style="font-weight: bold;"> Vinificazione e affinamento: Le uve Primitivo, vengono raccolte manualmente in piccoli cassoni e portate in cantina dove fermentano per circa 7 giorni. Sucessivamente il vino viene affinato in piccoli serbatoi di acciaio inox. Prima di essere messo in vendita, riposa in bottiglia per circa 2 mesi. </p>
-            </div>
-
-        </div>
-    </section>
-
-    <section class="container-fluid">
-
-        <div class="row">
-
-            <div class="col-12 col-md-4 d-flex justify-content-center">
-
-                <img src="media/casolare.jpg" class="img_casolare" alt="">
-
-            </div>
-
-        </div>
-
-    </section>
-
-
-    <!-- SEZIONE GOCCIA -->
-    <div class="container-fluid div_goccia">
-        
-        <div class="drop div_goccia"></div>
-        <div class="wave div_goccia"></div>
-        <div class="row div_goccia">
-            
-            <div class="col-12 div_goccia d-flex justify-content-center align-items-center vh-100">
                 
             </div>
+            
+            <div class="col-12 col-md-5">
+                
+                <p> IMMAGINE </p>
+                
+            </div>
+            
         </div>
+        
+    </section>
+    
+    
+    <!-- SECONDA SEZIONE SOTTO HEADER -->
+    <div class="container-fluid mt-5">
+        
+        <div class="row">
+            
+            <div class="col-12 col-md-5">
+                <p style="font-weight: bold;"> Vinificazione e affinamento: Le uve Primitivo, vengono raccolte manualmente in piccoli cassoni e portate in cantina dove fermentano per circa 7 giorni. Sucessivamente il vino viene affinato in piccoli serbatoi di acciaio inox. Prima di essere messo in vendita, riposa in bottiglia per circa 2 mesi. </p>
+            </div>
+            
+            <div class="col-12 col-md-5">
+                
+                <p> IMMAGINE </p>
+                
+            </div>
+            
+        </div>
+        
     </div>
     
+    <!-- TERZA SEZIONE SOTTO HEADER -->
     
-</x-layout>
+    <section class="container-fluid mt-5">
+        
+        <div class="row">
+            
+            <div class="col-12 col-md-5 d-flex justify-content-center">
+                
+                <i class="fa-solid fa-bottle-droplet fa-5x"></i>
+                
+            </div>
+            
+            <div class="col-12 col-md-5">
+                
+                <p>Caratteristiche Organolettiche
+                    
+                    Colore: Rosso porpora.
+                    
+                    Olfatto: Fruttato con note di mirtillo, ciliegia e prugna.
+                    
+                    Palato: Gusto fresco e morbido, avvolgente, di buona struttura e di pronta beva.
+                    
+                    
+                    
+                    Grado alcolico: 13,5% vol.
+                    
+                    Temperatura di servizio: 16-18 °C.</p>
+                    
+                </div>
+                
+            </div>
+            
+        </section>
+        
+        
+        <!-- SEZIONE GOCCIA -->
+        {{-- <div class="container-fluid div_goccia">
+            
+            <div class="drop div_goccia"></div>
+            <div class="wave div_goccia"></div>
+            <div class="row div_goccia">
+                
+                <div class="col-12 div_goccia d-flex justify-content-center align-items-center vh-100">
+                    
+                </div>
+            </div>
+        </div> --}}
+        
+        
+    </x-layout>

fa0a5fc78ef7ceeaf73e9402d1c974c50cf7ef76
Paolo Tozzi <paolo.tozzi98@libero.it>
2023-03-08 10:31:27 +0100

    ritocchi footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -4,19 +4,17 @@
         
         <div class="row footer_custom align-items-center">
             
-            <div class="col-12 col-md-7 p-5 text-center d-flex align-items-center justify-content-center flex-column">
-                <p class="text-white text-center"> Imbottigliato all'origine da: ICQRF TA/5617 </p>
-                <p class="text-white"> Mottola (TA) - Italia </p>
-                <p class="text-white"> Per FiRà srl </p>
-                <p class="text-white"> +39 3396347522 - Foggia </p>
+            <div class="col-12 col-md-7 text-center d-flex align-items-center justify-content-center flex-column">
+                <h5 class="text-white text-center"> Imbottigliato all'origine da: ICQRF TA/5617 Mottola (TA) - Italia</h5>
+                <h5 class="text-white"> Per FiRà srl +39 3396347522 - Foggia</h5>
             </div>
             
             
-            <div class="col-12 col-md-5 p-5 text-center d-flex align-items-center flex-column">
-                
-                <h5 class="text-white"> Seguici sulla nostra pagina Instagram </h5>
+            <div class="col-12 col-md-5 text-center d-flex align-items-center flex-column">
                 
                 <a href="" class="link_footer"><i class="fa-brands fa-instagram fa-3x"></i> </a>
+                
+                <h5 class="text-white"> Seguici sulla nostra pagina Instagram </h5>
                 
             </div>
             
